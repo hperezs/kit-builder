82a31a55056a6193d01f5f87d6b5f5bf56e3d399
Hugo Perez <h.perez.saldivar@gmail.com>
2021-03-30 16:44:23 -0600

    Made progress with self-made page and updating cart

--- components/steps/ChooseCables.js
+++ components/steps/ChooseCables.js
@@ -1,9 +1,10 @@
-import CableProduct from "./ChooseCables/cableProduct"
 import ChooseCable from "./ChooseCables/chooseCable"
 import SelfMadeProduct from "./ChooseCables/selfMadeProduct"
+import Image from 'next/image'
+import { backstreet_domain } from '../../lib/backstreet_domain'
 
 
-export default function ChooseCables({cablesType, cameras, indoorCables, outdoorCables, selfMadeProducts, selectCable}) {
+export default function ChooseCables({cablesType, cameras, indoorCables, outdoorCables, selfMadeProducts, selectCable, selectSMProducts, selectedSMProducts}) {
 
     if(cablesType == 'pre-made')return(
         <section className="my-10">
@@ -21,10 +22,45 @@ export default function ChooseCables({cablesType, cameras, indoorCables, outdoor
     if(cablesType == 'self-made')return(
         <section className="my-10">
             <div className="flex justify-center">
-                {selfMadeProducts.map(product => {
-                    return <SelfMadeProduct product={product} handleSelect={(product) => {console.log(product + ' selected.')}}/>
+                {selfMadeProducts.map((product, index) => {
+                    return <SelfMadeProduct product={product} selectSMProducts={selectSMProducts} key={index}/>
                 })}
             </div>
+
+           {selectedSMProducts.length != 0 && 
+                <div className="flex justify-center">
+                    <div className="flex flex-col items-center border rounded p-3">
+                        <h4 className="font-light text-xl mb-4">Your selections: </h4>
+                        <div className="flex justify-center ">
+                                {selectedSMProducts.map((product, index) => {
+                                    return(
+                                        <div className="flex flex-col justify-start items-center mx-5 mb-3 rounded p-3 border" key={index}>
+                                            <div className="m-2 p-5 flex flex-col justify-center items-center border rounded border-gray-300 ">
+                                                <div style={{height: '86px', width: '120px'}}> 
+                                                    <div style={{position: 'relative', maxWidth: '100%', height: '100%'}}>
+                                                        <Image
+                                                            src={backstreet_domain + '/pub/media/catalog/product' + product.media_gallery_entries[0].file}
+                                                            layout="fill"
+                                                            objectFit="contain"
+                                                            quality={100}
+                                                        />
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div className="flex flex-col items-center">
+                                                <p>{product.name}</p>
+                                                <p className="font-light">{product.sku} </p>
+                                                <p className="font-normal text-green-600">${product.price.toFixed(2)}</p>
+                                                <p>Qty: {product.quantity}</p>
+                                            </div>
+                                        </div>
+                                    )
+                                })}
+                        </div>
+                    </div>
+            
+                </div>
+            }
         </section>
     )
     
--- components/steps/ChooseCables/selfMadeProduct.js
+++ components/steps/ChooseCables/selfMadeProduct.js
@@ -3,11 +3,18 @@ import Image from 'next/image'
 import { useEffect, useState } from 'react';
 import { backstreet_domain } from '../../../lib/backstreet_domain'
 
-export default function SelfMadeProduct({product}) {
+export default function SelfMadeProduct({product, selectSMProducts}) {
     const [isLoading, setIsLoading] = useState(true);
+    const [quantity, setQuantity] = useState(1);
 
     const selectButton_styles = "px-5 py-1 border rounded bg-green-600 text-white text-sm uppercase tracking-wider font-semibold mt-3 transition hover:bg-green-400 focus:outline-none focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-500 ";
     const card_styles = "flex flex-col justify-start items-center my-14 mx-5 rounded p-3 border hover:shadow-lg hover:border-green-300 "
+
+    const handleSelect = product => {
+        product.quantity = quantity;
+        console.log('Adding to cart ' + product.quantity + ' ' + product.name);
+        selectSMProducts(product);
+    }
 
     return(
         <div className={card_styles}>
@@ -31,6 +38,28 @@ export default function SelfMadeProduct({product}) {
                 <p>{product.name}</p>
                 <p className="font-light">{product.sku} </p>
                 <p className="font-normal text-green-600">${product.price.toFixed(2)}</p>
+                <div className="flex justify-center items-center mt-3">
+                    <button
+                        onClick={e => setQuantity(quantity != 1 ? quantity - 1 : quantity)} 
+                        className="px-2 mr-1 border bg-gray-50 focus:outline-none shadow-sm focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50"
+                    >
+                        -
+                    </button>
+                    <input 
+                        type="number" 
+                        defaultValue={1} 
+                        min={1} 
+                        value={quantity} 
+                        onChange={e => setQuantity(e.target.value)}
+                        className="w-14 text-center mx-1 border-gray-300 shadow-sm focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50"
+                    />
+                    <button 
+                        onClick={e => setQuantity(quantity + 1)}
+                        className="px-2 ml-1 border bg-gray-50 focus:outline-none shadow-sm focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50"
+                    >
+                        +
+                    </button>
+                </div>
                 <button className={selectButton_styles} onClick={e => handleSelect(product)}>Add to cart</button>
             </div>
         </div>
