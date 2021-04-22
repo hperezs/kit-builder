855175fcd74397448cb4196f51f83e31241c9de2
Hugo Perez <h.perez.saldivar@gmail.com>
2021-04-22 14:38:54 -0600

    Changes to landing page

--- pages/index.js
+++ pages/index.js
@@ -47,10 +47,11 @@ export default function Home() {
                 <section className="welcome-container flex flex-col items-center w-screen border-b border-gray-400">
                     <div className="flex flex-col justify-center w-screen ">
                         <div style={{borderTopWidth: '5px'}} className="flex flex-col items-center justify-start border-green-600">
-                            <p style={{maxWidth: '900px'}} className="text-3xl text-center w-7/12 my-16">
-                                Build  your custom CCTV surveillance system in minutes, it's fun and FREE. Our Custom Kit Builder will guide you through each step, choosing the components that best fit your needs. No skill or previous knowledge about security cameras is needed.
+                            <h2 className="text-4xl font-semibold mt-14 text-green-800">Build Your Custom Surveillance System in Minutes</h2>
+                            <p style={{maxWidth: '850px'}} className="text-2xl text-center w-6/12 my-10">
+                                Our Custom Kit Builder will guide you through each step, choosing the components that best fit your needs. No skill or previous knowledge about security cameras is needed.
                             </p>
-                            <a style={{paddingBottom: '14px', paddingRight: '41px'}} onClick={handleClick} className="py-3 px-10 text-white border bg-green-700 bg-opacity-90 text-3xl rounded text-white text-center align-middle transition-colors duration-300 ease hover:bg-green-500 my`-8 cursor-pointer">
+                            <a style={{paddingBottom: '14px', paddingRight: '41px'}} onClick={handleClick} className="py-3 px-10 mb-5 text-white border bg-green-700 bg-opacity-90 text-3xl rounded text-white text-center align-middle transition-colors duration-300 ease hover:bg-green-500 my`-8 cursor-pointer">
                                 Get started
                             </a>
                             <Link href="/guide"><span className="-z-50 opacity-0" id='get-started-link'>get started link</span></Link>
@@ -72,7 +73,7 @@ export default function Home() {
                 {/* How it works section */}
                 <section className="mx-20 my-14 p-10 flex justify-center">
                     <div style={{maxWidth: '600px'}} className="">
-                        <h3 className="text-3xl mb-8 font-semibold">How it works</h3>   
+                        <h3 className="text-3xl mb-8 font-semibold text-green-900">How it works</h3>   
                         <p className="text-xl">The experts at Backstreet Surveillance have applied their 57+ years of experience and created this beginner-friendly CCTV design tool. The Custom Kit Builder simplifies the complex process of assembling the perfect video surveillance system.  <br/> <br/> We have removed the hassle of researching and finding each individual component. Our Kit Builder allows you to design your ideal system in a matter of minutes, then proceed to purchase or have a line item quote sent to your email, you choose.</p>
                     </div>
                     <div style={{height: '400px', width: '600px'}} className="ml-20"> 
@@ -88,7 +89,7 @@ export default function Home() {
                 </section>
 
                 <section className="my-14 py-20 border-t border-b border-gray-400">
-                    <h3 className="text-center text-3xl mb-10 font-semibold">What's included</h3>
+                    <h3 className="text-center text-3xl mb-10 font-semibold text-green-900">What's included</h3>
                     <p className="text-xl text-center">We make sure you don't miss any of the essential components you'll need.</p>
                     <div className="flex justify-center">
                         <div className="flex justify-center mt-20 md:w-10/12 lg:w-8/12 xl:w-10/12 flex-wrap">
@@ -215,7 +216,7 @@ export default function Home() {
                         </div>
                     </div>
                     <div style={{maxWidth: '600px'}} className="mt-10">
-                        <h3 className="text-3xl font-semibold mb-8">Why purchase from Backstreet?</h3>   
+                        <h3 className="text-3xl font-semibold mb-8 text-green-900">Why purchase from Backstreet?</h3>   
                         <p className="text-xl">Backstreet is the affordable solution for easy to use professional surveillance equipment. We are an American owned company and all of our support representatives and facilities are located in the U.S. We only offer ultra high definition equipment and we back it with a 5 year warranty and free lifetime technical support. The Backstreet promise says it all: “We promise that you will not find a lower price for the same quality and support, anywhere.”</p>
                     </div>
                 </section>
@@ -223,7 +224,7 @@ export default function Home() {
                 <section className="mx-20 p-10 col items-center">
                     <div className="flex justify-center items-center mb-10">
                         <div style={{maxWidth: '600px'}}>
-                            <h5 className="text-2xl font-semibold">30-Day Money Back Guarantee</h5>
+                            <h5 className="text-2xl font-semibold text-green-900">30-Day Money Back Guarantee</h5>
                             <p className="text-lg mt-5">Any item can be returned within 30 days from the date of delivery for a refund. If you need to change an item, we will be glad to exchange any item for another. If the equipment is just not right for you then we will provide a 100% refund once we receive the equipment and all materials back. </p>
                         </div>
                         <div style={{height: '225px', width: '280px'}} className="ml-20"> 
@@ -239,7 +240,7 @@ export default function Home() {
                     </div>    
                     <div className="flex justify-center items-center mb-10">
                         <div style={{maxWidth: '600px'}}>
-                            <h5 className="text-2xl font-semibold">Longest Warranty in the Industry</h5>
+                            <h5 className="text-2xl font-semibold text-green-900">Longest Warranty in the Industry</h5>
                             <p className="text-lg mt-5">All equipment is covered under a complete 5 year warranty from the date of delivery. During this time all products are guaranteed against manufacturing and operational defects. In the event of such defects during this period, it is our responsibility to replace the defective item with an exact or comparable product.</p>
                         </div>
                         <div style={{height: '225px', width: '280px'}} className="ml-20"> 
@@ -255,10 +256,10 @@ export default function Home() {
                     </div> 
                     <div className="flex justify-center items-center mb-10">
                         <div style={{maxWidth: '600px'}}>
-                            <h5 className="text-2xl font-semibold">USA Based - Unlimited Technical Support</h5>
+                            <h5 className="text-2xl font-semibold text-green-900">USA Based - Unlimited Technical Support</h5>
                             <p className="text-lg mt-5">We provide USA based, Free, Unlimited, Life-time Technical Support for all the equipment that we offer.</p>
                         </div>
-                        <div style={{height: '200px', width: '260px'}} className="ml-20"> 
+                        <div style={{height: '200px', width: '280px'}} className="ml-20"> 
                             <div style={{position: 'relative', maxWidth: '100%', height: '100%'}}>
                                 <NextImage
                                     src={"/images/BasedInTheUSA.png"}
@@ -271,8 +272,8 @@ export default function Home() {
                     </div>                     
                 </section>
                 <section className="flex flex-col items-center justify-center bg-green-700 text-gray-50 py-12">
-                    <h5 className="need-help tracking-wider ">Need Help Learning About Cameras?</h5>
-                    <a className="text-xl mt-5 cursor-pointer hover:text-green-100">View helpful links</a>
+                    <h5 className="need-help">Need More Information About Security Cameras?</h5>
+                    <a href="https://backstreet-surveillance.com/blog" target="_blank" className="text-xl mt-5 cursor-pointer hover:text-green-100">View our security blog</a>
                 </section>
                 <footer style={{backgroundColor: '#494949'}} className="flex justify-between items-center justify-center w-full py-2 px-10">
                     {/* Icons */}
