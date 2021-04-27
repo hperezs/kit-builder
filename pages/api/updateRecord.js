24e9405a141470bd62aed161bbfccf9c70c4d00b
Hugo Perez <h.perez.saldivar@gmail.com>
2021-04-27 10:03:24 -0600

    Updated analytic api

--- pages/api/updateRecord.js
+++ pages/api/updateRecord.js
@@ -14,7 +14,8 @@ export default async function UpdateRecord(req, res) {
             .collection('user-records')
             .updateOne({"ipAddress": req.query.ipAddress}, { $set: {hasStarted: true} })
         
-        res.status(200).send('Review step reached.');
+        console.log('Builder started');
+        res.status(200).send('Builder started.');
     }
 
     if(req.query.step == 'review'){
@@ -23,7 +24,18 @@ export default async function UpdateRecord(req, res) {
             .collection('user-records')
             .updateOne({"ipAddress": req.query.ipAddress}, { $set: {hasReachedReview: true} })
         
+        console.log('Customer reached review step.');
         res.status(200).send('Review step reached.');
+    }
+
+    if(req.query.step == 'end'){
+        let result = await client
+            .db('traffic')
+            .collection('user-records')
+            .updateOne({"ipAddress": req.query.ipAddress}, { $set: {proceededToCart: true} })
+        
+            console.log('Customer proceeded to cart.')
+        res.status(200).send('Proceeded to cart.');
     }
     
 }
