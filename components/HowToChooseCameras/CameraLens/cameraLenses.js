19e962cc007e6a9dd526defd35294d5e0d399e08
Hugo Perez <h.perez.saldivar@gmail.com>
2021-06-04 12:04:01 -0600

    Added zoom lens explanation

--- components/HowToChooseCameras/CameraLens/cameraLenses.js
+++ components/HowToChooseCameras/CameraLens/cameraLenses.js
@@ -11,7 +11,17 @@ export default function CameraLenses() {
                     <br/>
                     <ul className="list-inside">
                         <li className="mb-4 p-4 border rounded shadow">If the viewing area is <span className="text-green-600 font-semibold">under 60 feet</span> from the camera, a <span className="text-green-600 font-semibold">3.6mm fixed lens</span> is recommended. This lens provides a wide angle for close objects.</li>
-                        <li className="mb-4 p-4 border rounded shadow">If the viewing area is <span className="text-green-600 font-semibold">over 60 feet</span> from the camera, a <span className="text-green-600 font-semibold">2.8-12mm zoom lens</span> is recommended. The lens can be adjusted to provide clear images for close objects or for longer distances.</li>
+                        <li className="mb-4 p-4 border rounded shadow">If the viewing area is <span className="text-green-600 font-semibold">over 60 feet</span> from the camera, a <span className="text-green-600 font-semibold">2.8-12mm zoom lens</span> is recommended. The lens can be adjusted to provide clear images for close objects or for longer distances. This lense can be <span className="font-semibold text-green-600">manual</span> or <span className="font-semibold text-green-600">motorized</span>.</li>
+                        <div className="flex text-center">
+                            <li className="mb-4 p-4 border rounded shadow mr-2">
+                                <h5 className="text-green-600 font-semibold">Manual</h5>
+                                <p>This lens can only be manually adjusted by hand</p>
+                            </li>
+                            <li className="mb-4 p-4 border rounded shadow ml-2">
+                                <h5 className="text-green-600 font-semibold">Motorized</h5>
+                                <p>This lens can be adjusted on the Video Recorder through software that controls the lens motor.</p>
+                            </li>
+                        </div>
                         <li className="mb-4 p-4 border rounded shadow">If the viewing area is <span className="text-green-600 font-semibold">over 150 feet</span> from the camera, consider <span className="text-green-600 font-semibold">PTZ cameras</span> with a motorized zoom lens.</li>
                     </ul>
                 </div>
