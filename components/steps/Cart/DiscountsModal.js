af56b960950c752826ae68ca95e3f29f4a9084d9
Hugo Perez <h.perez.saldivar@gmail.com>
2021-06-28 08:14:35 -0600

    Added new promo code

--- components/steps/Cart/DiscountsModal.js
+++ components/steps/Cart/DiscountsModal.js
@@ -34,9 +34,9 @@ export default function DiscountsModal({proceedToPurchase}) {
                     <div className="mx-3 sm:text-lg lg:text-xl flex flex-col items-center">
                         <p className="lg:text-2xl mb-7">Your selections qualify for a discount!</p>
                         
-                        <Image src="/images/promo-code.jpg" width={600} height={300} />
+                        <Image src="/images/Summer2021PromoCode.jpg" width={600} height={300} />
 
-                        <div style={{maxWidth: "400px"}} className="text-lg mt-5 text-center"><span className="text-green-600 text-2xl font-light mr-2">&#8594;</span>Head over to our checkout page and use the code <span className="text-green-600 font-semibold">SPRINGGREEN</span> for a discount.</div>
+                        <div style={{maxWidth: "400px"}} className="text-lg mt-5 text-center"><span className="text-green-600 text-2xl font-light mr-2">&#8594;</span>Head over to our checkout page and use the code <span className="text-green-600 font-semibold">SAFESUMMER</span> for a discount.</div>
 
                         <div className="p-4 border mt-5 shadow w-11/12 border-green-400 border-opacity-80">
                           <span className="text-green-600 font-semibold mr-1">Note:</span> You can choose to send a quote of your selections to your email or finish placing your order
